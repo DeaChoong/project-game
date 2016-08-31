6577075663d5b5ae4f12e51e1cee0749853d460a
KimChan <azs7123@gmail.com>
2016-08-31 21:02:25 +0900

    0.0.2

--- project-game/routes/index.js
+++ project-game/routes/index.js
@@ -6,13 +6,4 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Project-Game Main' });
 });
 
-router.get('/input', function(req, res, next) {
-	res.render('input', { title: 'Project-Game Main' });
-});
-
-router.post('/output', function(req, res, next) {
-	res.render('output', { title: 'Project-Game Main' });
-	res.render('output', { user_id: req.body.user_id });
-});
-
 module.exports = router;
